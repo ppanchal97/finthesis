beae89e058222d3a24d034bbc2f71700e0ca4373
Parik Panchal <parikspanchal@icloud.com>
2024-08-06 14:30:39 -0400

    fixed type check warning

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,8 +1,8 @@
 // pages/_document.tsx
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
